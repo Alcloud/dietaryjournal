49d12507fc20b500827a1ab92dd5bda8ca758a6a
Piatkin <aleksei.piatkin@fokus.fraunhofer.de>
2020-01-18 22:06:34 +0100

    init commit

---
+++ src/test/java/com/alcloud/dietaryjournal/NativeFoodResourceIT.java
@@ -0,0 +1,9 @@
+package com.alcloud.dietaryjournal;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeFoodResourceIT extends FoodResourceTest {
+
+    // Execute the same tests but in native mode.
+}
