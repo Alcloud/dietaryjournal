9b4f65ac338b3cc9de944b7123a02ef0ce233714
Piatkin <aleksei.piatkin@fokus.fraunhofer.de>
2020-01-19 17:13:49 +0100

    create FoodService class

---
+++ src/main/java/com/alcloud/dietaryjournal/FoodService.java
@@ -0,0 +1,38 @@
+package com.alcloud.dietaryjournal;
+
+import com.alcloud.dietaryjournal.model.Food;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FoodService {
+
+    private static FoodService ourInstance = new FoodService();
+
+    public static FoodService getInstance(){
+        return ourInstance;
+    }
+
+    private Map<Long, Food> foods = new HashMap<>();
+
+    List<Food> getAllFood(){
+        return new ArrayList<>(foods.values());
+    }
+
+    Long addFood(Food food){
+        long id = (foods.size() + 1);
+        food.setId(id);
+        foods.put(food.getId(), food);
+        return id;
+    }
+
+    Food removeFood(long id){
+        return foods.remove(id);
+    }
+
+    public Food getFoodById(long id) {
+        return foods.get(id);
+    }
+}
