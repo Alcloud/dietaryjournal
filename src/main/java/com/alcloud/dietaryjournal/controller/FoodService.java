dd2715f2a93be90981ced96c405b51e1dc7e71aa
Piatkin <aleksei.piatkin@fokus.fraunhofer.de>
2020-01-20 20:15:53 +0100

    add logger and exception

--- src/main/java/com/alcloud/dietaryjournal/controller/FoodService.java
+++ src/main/java/com/alcloud/dietaryjournal/controller/FoodService.java
@@ -9,9 +9,9 @@ import java.util.*;
 /**
  * The FoodService class implements CRUD operations for food items
  *
- * @author  Aleksei Piatkin
+ * @author Aleksei Piatkin
  * @version 1.0
- * @since   2020-01-19
+ * @since 2020-01-19
  */
 
 class FoodService {
@@ -30,22 +30,25 @@ class FoodService {
     }
 
     /**
-     * @param userId - user id
-     * @param dateLow - date begin
+     * @param userId   - user id
+     * @param dateLow  - date begin
      * @param dateHigh - end date
      * @return list of the sorted by date food items
      */
-    List<Food> getAllFood(String userId, Date dateLow, Date dateHigh){
+    List<Food> getAllFood(String userId, Date dateLow, Date dateHigh) {
         List<Food> sortedByDateFood = new ArrayList<>();
-        for (Food food: foods.values()){
+        for (Food food : foods.values()) {
 
             // Check if the request matches the specified time period and user.
-            if(food.getUserId().equals(userId) &&
-                    food.getDate().compareTo(dateLow) > 0 &&
-                    food.getDate().compareTo(dateHigh) < 0){
-                sortedByDateFood.add(food);
+            if (food.getUserId() != null && dateLow != null && dateHigh != null) {
+                if (food.getUserId().equals(userId) &&
+                        food.getDate().compareTo(dateLow) > 0 &&
+                        food.getDate().compareTo(dateHigh) < 0) {
+                    sortedByDateFood.add(food);
+                }
             }
         }
+        logger.debug("Sorted food for user " + userId + ": " + sortedByDateFood.toString());
         return sortedByDateFood;
     }
 
@@ -53,19 +56,15 @@ class FoodService {
      * @param food - food item
      * @return id of added food item
      */
-    Long addFood(Food food){
-        long id = (foods.size() + 1);
-        food.setId(id);
+    Long addFood(Food food) throws IllegalArgumentException {
 
         // Check if amount is negative
-        if (food.getPortion() < 0){
-            food.setPortion(0);
-            logger.warn("The portion must be positive");
+        if (food.getPortion() < 0 || food.getWeight() < 0) {
+            throw new IllegalArgumentException("The amount must be positive");
         }
-        if (food.getWeight() < 0){
-            food.setWeight(0);
-            logger.warn("The weight must be positive");
-        }
+
+        long id = (foods.size() + 1);
+        food.setId(id);
 
         foods.put(food.getId(), food);
         return id;
@@ -75,7 +74,7 @@ class FoodService {
      * @param id - food id
      * @return deleted food item
      */
-    Food removeFood(long id){
+    Food removeFood(long id) {
         return foods.remove(id);
     }
 }
