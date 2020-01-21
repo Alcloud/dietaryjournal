8e98e06d4c927784e4c4a6774b5fb7fea7402d65
Piatkin <aleksei.piatkin@fokus.fraunhofer.de>
2020-01-21 14:13:36 +0100

    Update resource test

--- src/test/java/com/alcloud/dietaryjournal/controller/FoodResourceTest.java
+++ src/test/java/com/alcloud/dietaryjournal/controller/FoodResourceTest.java
@@ -2,16 +2,40 @@ package com.alcloud.dietaryjournal.controller;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.MediaType;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
 @QuarkusTest
 class FoodResourceTest {
 
     @Test
-    void testGetFood() {
+    public void testAdd() {
         given()
-          .when().get("/1")
-          .then()
-             .statusCode(200);
+                .body("{\n" +
+                        "        \"date\": \"2020-01-21T12:58:49.249Z[UTC]\",\n" +
+                        "        \"id\": 1,\n" +
+                        "        \"name\": \"apple\",\n" +
+                        "        \"portion\": 2,\n" +
+                        "        \"userId\": \"4\",\n" +
+                        "        \"weight\": 300\n" +
+                        "    }")
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .when()
+                .post("/foods")
+                .then()
+                .statusCode(200);
+    }
+
+
+    @Test
+    public void testList() {
+        given()
+                .when().get("/foods/4")
+                .then()
+                .statusCode(204);
     }
 }
